5344c1af0e11f76a46cd7e7a624b4ff27b272d3e
captain-mike <info@grafikweb.it>
2022-10-28 18:02:22 +0200

    js basi

---
+++ m1/w2/d1/js1/script.js
@@ -0,0 +1,22 @@
+/*
+Questo è un commento desu più righe
+*/
+
+//commento ad una riga
+
+/***********************************************************
+ * Direttive di output
+ */
+
+
+//alert('mostra questa scritta')
+console.log('Mostra questa scritta');
+console.log(3 + 2);
+
+console.log('%c Mostra questa scritta','color:red;background-color:blue');
+
+document.getElementById('test').innerHTML = '<b>Prova</b>';
+
+document.write('Ciao')//attenzione, potrebbe eliminare il contenuto della pagina
+
+
